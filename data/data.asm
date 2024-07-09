1c64bbe9de69907145037fd24a38597d761d72ba
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-10 00:49:41 +1000

    Create data.asm

---
+++ data/data.asm
@@ -0,0 +1,38 @@
+; This file contains various buffers and definitions used by the assembler
+
+
+%define LINE_BUFFER_SIZE 1000
+section .data
+
+	; Keys
+	; Instructions
+	key_add: db "add" ; Add instruction
+	key_lir: db "lir" ; Load immediate instruction
+	key_psh: db "psh" ; Push instruction	
+	key_pop: db "pop" ; Pop instruction
+	key_jeq: db "jeq" ; Jump if equal instruction
+	key_blk: db "blk" ; Blank marker -> value = 0
+
+	; Registers
+	key_eax: db "eax" ; eax register
+	key_ebx: db "ebx" ; ebx register	
+	key_ecx: db "ecx" ; ecx register
+	key_edx: db "edx" ; edx register
+	
+
+
+	; Values - Machine code
+	; Instrurctions
+
+
+	; Registers
+
+
+section .bss
+	
+	line_buffer: resb LINE_BUFFER_SIZE
+	
+
+
+
+
