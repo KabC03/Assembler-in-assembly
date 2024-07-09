5936ccf8934fc9ebe41aacac9114064bbe951004
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-10 00:16:26 +1000

    Create djb2.asm

---
+++ hashmap/djb2.asm
@@ -0,0 +1,70 @@
+%define HASH_TABLE_SIZE 100
+%define KEY_LENGTH 3
+
+%define DJB2_HASH_SEED 5381
+
+section .data
+	str: db "poo"
+section .text
+	global _start
+
+
+
+_start:
+
+
+
+	lea ebx, str
+	call djb2_hash
+
+
+	; Exit program
+	mov eax, 1
+	mov ebx, ecx
+	int 0x80
+
+
+; djb2_hash
+; 
+; Brief: Hashes a key of length KEY_LENGTH for a table of size HASH_TABLE_SIZE
+;
+; ebx -> Pointer to string to hash
+; ecx -> Location of where the hash result is stored
+djb2_hash:
+
+	mov ebp, esp
+	push eax	
+	push edx
+	
+	mov ecx, DJB2_HASH_SEED
+	
+	mov eax, 0
+FOR_START:	
+	cmp eax, KEY_LENGTH
+	je FOR_END
+
+	imul ecx, 33
+
+	; Add char byte
+	movzx edx, byte [ebx + eax]	
+	add ecx, edx
+
+
+	inc eax
+	jmp FOR_START
+FOR_END:
+
+	push ebx
+	; Perform modulo
+	mov eax, ecx
+	xor edx, edx
+	mov ebx, HASH_TABLE_SIZE
+	div ebx 
+	mov ecx, edx
+	pop ebx	
+
+	mov esp, ebp
+	ret
+
+
+
