639ca8baec2b18d9fe20ff07fec70376528afe9b
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-11 22:39:09 +1000

    Update generate data.py

--- data/generate data.py
+++ data/generate data.py
@@ -1,9 +1,9 @@
-#Generates the data section for the asm program
+#Generates the data section for the asm program - also write the hash table in assembly
 
 import sys;
 
 dataFileName = "data.asm";
-
+djb2Hash = 5381; #Will get changed by perfect hash function generator
 
 preAppendDefines = {
 
@@ -16,6 +16,21 @@ preAppendDefines = {
     "DJB2_HASH_SEED" : 5381,
 };
 
+
+#Number of bytes required for inidividual items
+pneumonicLength = 3;
+machineCodeLength = 1;
+sourceCodePneumonocs = {
+
+    #Pneumonic : Machine code
+
+    #Instructions
+    "ADD" : "0", "LIR" : "1", "PSH" : "2", "POP" : "3", "JEQ" : "4", "BLK" : "-1",
+
+    #Registers
+    "EAX" : "0", "EBX" : "1", "ECX" : "2", "EDX" : "3",
+
+};
 
 
 
@@ -38,13 +53,37 @@ def write_defines():
 
 
 
-#Write data section
+#Write .data section
 def write_data_section() :
     try:
         with open(dataFileName, 'a') as file:
 
             file.write("; .data section\n");
             file.write("section .data\n");
+
+
+            file.write("\n\n\n");
+        return True;
+    except:
+        return False;
+
+
+#Write .bss section
+def write_bss_section():
+
+
+    try:
+        with open(dataFileName, 'a') as file:
+
+            file.write("; .bss section\n");
+            file.write("section .bss\n");
+
+            #Space for key hashmap
+            #file.write("    keyBuffer: db " + str(len(sourceCodePneumonocs.keys()) * pneumonicLength) + "\n"); #Keys dont need to be stored
+            file.write("    valueBuffer: db " + str(len(sourceCodePneumonocs.values()) * machineCodeLength) + "\n");
+
+
+
 
         return True;
     except:
@@ -58,12 +97,13 @@ def main():
     if(write_defines() == False):
         print("Failed to write defines section\n");
         return -1;
-
-
-
     if(write_data_section() == False):
-        print("Failed to write data section\n");
+        print("Failed to write .data section\n");
         return -2;
+    if(write_bss_section() == False):
+        print("Failed to write .bss section");
+        return -3;
+
 
 
     return 0;
@@ -72,7 +112,6 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main());
-
 
 
 
