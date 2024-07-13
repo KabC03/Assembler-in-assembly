2eb2ee94baec99901eec558ca13165e81dd998de
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-13 23:50:45 +1000

    Update data.asm

--- data/data.asm
+++ data/data.asm
@@ -1,133 +1,23 @@
-#Generates the data section for the asm program - also write the hash table in assembly
+; Defines section
 
-import sys;
+%define LINE_BUFFER_SIZE 1000
 
-dataFileName = "data.asm";
 
 
-preAppendDefines = {
+; .data section
+section .data
 
-    #File processing
-    "LINE_BUFFER_SIZE" : 1000, 
 
-    #Hashmap
-    "HASH_TABLE_SIZE" : 100,
-    "KEY_LENGTH" : 3,
-    "DJB2_HASH_SEED" : 5381,
-};
 
+; .bss section
+section .bss
+    valueBuffer: db 10
+; .text section
+section .text
 
-#Number of bytes required for inidividual items
-pneumonicLength = 3;
-machineCodeLength = 1;
-sourceCodePneumonocs = {
-
-    #Pneumonic : Machine code
-
-    #Instructions
-    "ADD" : "0", "LIR" : "1", "PSH" : "2", "POP" : "3", "JEQ" : "4", "BLK" : "-1",
-
-    #Registers
-    "EAX" : "0", "EBX" : "1", "ECX" : "2", "EDX" : "3",
-
-};
-
-
-
-def write_defines():
-    try:
-        with open(dataFileName, 'w') as file:
-
-            file.write("; Defines section\n\n");
-
-            for key, value in preAppendDefines.items():
-                file.write("%define " + str(key) + " " + str(value) + "\n");
-
-
-            file.write("\n\n\n");
-
-        return True;
-    except:
-        return False;
-
-
-
-
-#Write .data section
-def write_data_section() :
-    try:
-        with open(dataFileName, 'a') as file:
-
-            file.write("; .data section\n");
-            file.write("section .data\n");
-
-
-            file.write("\n\n\n");
-        return True;
-    except:
-        return False;
-
-
-#Write .bss section
-def write_bss_section():
-
-
-    try:
-        with open(dataFileName, 'a') as file:
-
-            file.write("; .bss section\n");
-            file.write("section .bss\n");
-
-            #Space for key hashmap
-            file.write("    keyBuffer: db " + str(len(sourceCodePneumonocs.keys()) * pneumonicLength) + "\n");
-            file.write("    valueBuffer: db " + str(len(sourceCodePneumonocs.values()) * machineCodeLength) + "\n");
-
-
-
-
-        return True;
-    except:
-        return False;
-
-
-
-def main():
-
-    #MUST call this first - since it opens for writing NOT appending
-    if(write_defines() == False):
-        print("Failed to write defines section\n");
-        return -1;
-    if(write_data_section() == False):
-        print("Failed to write .data section\n");
-        return -2;
-    if(write_bss_section() == False):
-        print("Failed to write .bss section");
-        return -3;
-
-
-
-    return 0;
-
-
-
-if __name__ == "__main__":
-    sys.exit(main());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+initialise_hashmap:
+    push eax
+    lea edi, [valueBuffer]
+    mov dword [edi], 65686876
+    mov dword [edi], 73828083
+    mov word [edi], 7280
